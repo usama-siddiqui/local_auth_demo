c28d6f7a31ea42987b37ee08dc3906c24f70ae11
muhammadusamasiddiqui <usama.siddiqui@technado.co>
2021-11-26 14:44:46 +0500

    Initial Commit

---
+++ lib/page/home_page.dart
@@ -0,0 +1,43 @@
+   
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+import 'fingerprint_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(32),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Home',
+                  style: TextStyle(fontSize: 40),
+                ),
+                SizedBox(height: 48),
+                buildLogoutButton(context)
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Widget buildLogoutButton(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size.fromHeight(50),
+        ),
+        child: Text(
+          'Logout',
+          style: TextStyle(fontSize: 20),
+        ),
+        onPressed: () => Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => FingerprintPage()),
+        ),
+      );
+}
